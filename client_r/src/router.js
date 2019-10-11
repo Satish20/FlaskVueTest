ce7fa3cc16478a2f6d97bcaed619b9034d1c4347
Satish <satishbiharie@gmail.com>
2019-10-11 16:57:27 +0200

    Added Update/Delete/Alerts

--- client_r/src/router.js
+++ client_r/src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Ping from './components/Ping.vue';
 import Books from './components/Books.vue';
 
 Vue.use(Router);
@@ -13,11 +12,6 @@ export default new Router({
       path: '/',
       name: 'Books',
       component: Books,
-    },
-    {
-      path: '/ping',
-      name: 'Ping',
-      component: Ping,
     },
   ],
 });
--- server/README.md
+++ server/README.md
@@ -1,6 +1,15 @@
 # server
 
 ## Project setup
+Activate virtual environment
+```
+env/Scripts/activate
+```
+Install dependencies
 ```
 pip install -r requirements.txt
+```
+Start app
+```
+python app.py
 ```
